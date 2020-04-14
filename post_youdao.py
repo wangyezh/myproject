73d28d3c29c3d26da7fdefde687d77e5f41c8ef8
Your Name <you@example.com>
2020-04-14 09:55:12 +0800

    作业6 1-wangyezheng finish post data

--- post_youdao.py
+++ post_youdao.py
@@ -1,7 +1,8 @@
 import random
 import requests
 import time
-
+import  json
+import hashlib
 class Youdao():
     def __init__(self,content):
         self.content=content
@@ -10,29 +11,21 @@ class Youdao():
         self.salt=self.get_salt()
         self.sign=self.get_sign()
     def get_salt(self):
-        s=str(random.randint(0,10))
-        t=self.ts
-
-        return t+s
+        return self.ts + str(random.randint(0, 10))
 
 
     def get_md5(self,value):
-        import hashlib
         m = hashlib.md5()
         m.update(value.encode("utf-8"))
         return m.hexdigest()
 
     def get_sign(self):
-        i=self.salt
-        e=self.content
-        s="fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
-
+        s = "fanyideskweb" + self.content + self.salt + "Nw(nmmbP%A-r6U3EUn]Aj"
         return self.get_md5(s)
 
 
     def get_ts(self):
-        t = time.time()
-        ts=str(int(round(t*1000)))
+        ts = str(int(round(time.time() * 1000)))
         return ts
 
     def yield_form_data(self):
@@ -53,20 +46,23 @@ class Youdao():
         }
         return form_data
     def get_headers(self):
-        headers={
+        return {
                     'Cookie': 'OUTFOX_SEARCH_USER_ID=461655988@10.108.160.17; JSESSIONID=aaaFE8--wD_8--zByeFfx; OUTFOX_SEARCH_USER_ID_NCOO=2098111493.268126; ___rl__test__cookies=1586775128482',
                     'Referer': 'http://fanyi.youdao.com/',
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36',
         }
-        return headers
+
 
 
     def fanyi(self):
         response = requests.post(self.url, data=self.yield_form_data(), headers=self.get_headers())
-        return response.text
+        content=json.loads(response.text)
+        return content['translateResult'][0][0]['tgt']
 
 
 if __name__ == '__main__':
-    youdao=Youdao('我们')
-    print(youdao.fanyi())
+    while(True):
+        i=input("please input:")
+        youdao=Youdao(i)
+        print(youdao.fanyi())
 
